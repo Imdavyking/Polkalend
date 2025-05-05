b00cfc25fc5ed2038dbc8b67a1c2132ac210104b
imdavyking <yungdynamic53@gmail.com>
2025-05-05 19:29:12 +0100

    chore: updated polkadot

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,27 @@ import NavHeader from "./components/NavHeader";
 import Router from "./router";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { UseInkProvider } from "useink";
+import { RococoContractsTestnet } from "useink/chains";
+import metadata from "./assets/json/polkalend.json";
 
 function App() {
   return (
-    <>
+    <UseInkProvider
+      config={{
+        dappName: metadata.contract.name,
+        chains: [RococoContractsTestnet],
+        caller: {
+          default: "5EyR7vEk7DtvEWeefGcXXMV6hKwB8Ex5uvjHufm466mbjJkR",
+        },
+      }}
+    >
       <ToastContainer />
       <BrowserRouter>
         <NavHeader />
         <Router />
       </BrowserRouter>
-    </>
+    </UseInkProvider>
   );
 }
 
